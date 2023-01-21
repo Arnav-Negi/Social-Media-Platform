8c8d47d40dfa4cb90d9eb53f758cc709a191c554
Arnav <arnavnegi14@gmail.com>
2023-01-21 19:22:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const name = "Arnav";
+  // let count = 0;
+
+  let [countState, setCountState] = useState(0);
+
+  const onClick = () => { 
+    setTimeout( () => setCountState((c) => c + 1), 1000);
+  }
+
+  console.log("function running", countState);
+  useEffect(() => {
+    console.log("update", countState);
+
+    window.addEventListener("scroll", () => console.log("scroll", countState));
+  }, [countState]);
+
+  return (<div style={{height: "2000px", backgroundColor : "red"}}>
+     <button onClick={onClick}>{countState}</button>;
+  </div>);
+ 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello {name}!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
