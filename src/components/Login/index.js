19f0b7f56a75567ed9dc60178ac8b0cafd939070
Arnav <arnavnegi14@gmail.com>
2023-01-22 12:06:45 +0530

    testing react

---
+++ src/components/Login/index.js
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <section className='h-screen'>
+            <div className='grid place-items-center h-screen'>
+                Centered text
+            </div>
+        </section>
+    )
+}
