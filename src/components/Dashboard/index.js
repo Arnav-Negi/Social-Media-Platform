bf5a6831bf11643ff80cb4d706e25b3fff19f749
Arnav <arnavnegi14@gmail.com>
2023-01-26 15:53:16 +0530

    part 1 done

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,25 +1,36 @@
 import Navbar from "./Navbar";
 import Userinfo from "./Userinfo";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 export default function Dashboard() {
     const navigate = useNavigate();
+    const [Authenticated, setAuthenticated] = useState(false);
 
     useEffect(() => {
         const data = localStorage.getItem("session");
         console.log(data)
-        if (data === null || data === false)
+        if (data === null || data === "false")
             navigate("/")
+        else {
+            setAuthenticated(true)
+        }
     }, []);
 
-    return (
-        <div>
-            <Navbar />
-            <div className={"bg-orange-400 h-screen flex place-items-center justify-center"}>
-                <Userinfo />
+    if (Authenticated)
+        return (
+            <div>
+                <Navbar/>
+                <div className={"bg-orange-400 h-screen flex place-items-center justify-center"}>
+                    <Userinfo/>
+                </div>
+                <div className={"bg-red-300 h-screen"}></div>
             </div>
-            <div className={"bg-red-300 h-screen"}></div>
-        </div>
-    )
+        )
+    else
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
 }
